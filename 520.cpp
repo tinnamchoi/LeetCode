ea1847c20a0b8634568366df949054e1218c7ca8
tinnamchoi <tinnam.choi@gmail.com>
2023-01-02 18:34:00 +0800

    520-A

--- 520.cpp
+++ 520.cpp
@@ -10,15 +10,20 @@ class Solution {
 public:
     bool detectCapitalUse(string word) {
         bool allupper = true;
+        bool hasupper = false;
         if (isupper(word[0]) == 0) { // if is lower
             allupper = false;
         }
         for (int i = 1; i < word.length(); i++) {
             if (isupper(word[i]) != 0) { // if is upper
+                hasupper = true;
                 if (allupper == false) {
                     return false;
                 }
-            } else {
+            } else { // is lower
+                if (i >= 2 && hasupper == true) {
+                    return false;
+                }
                 allupper = false;
             }
         }
