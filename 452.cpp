8ff1390fd00cca10535ac6c5cd41e67f824fcc82
tinnamchoi <tinnam.choi@gmail.com>
2023-01-06 01:28:00 +0800

    452-TLE

--- 452.cpp
+++ 452.cpp
@@ -10,20 +10,25 @@ class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
         sort(points.begin(), points.end());
+        for (auto x : points) {
+            cout << x[0] << " " << x[1] << endl;
+        }
+        cout << endl;
         int count = 1;
         int temp = 0;
         for (int i = 1; i < points.size(); i++) {
             temp++;
             bool inc = false;
-            for (int j = 0; j < temp; j++) {
-                if (points[i][0] > points[i - j - 1][1]) {
+            for (int j = 1; j <= temp; j++) {
+                if (points[i][0] > points[i - j][1]) {
                     inc = true;
                 }
             }
             if (inc) {
                 count++;
-                temp = 1;
+                temp = 0;
             }
+            cout << temp << " " << count << endl;
         }
         return count;
     }
