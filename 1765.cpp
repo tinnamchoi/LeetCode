170bcdbe87208928dd63ae6aa913693d10b09b96
tinnamchoi <tinnam.choi@gmail.com>
2023-01-06 19:05:00 +0800

    1765-TLE

--- 1765.cpp
+++ 1765.cpp
@@ -10,33 +10,28 @@ class Solution {
 public:
     vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
         vector<vector<int>> height = isWater;
+        vector<vector<int>> waters;
         int m = height.size();
         int n = height[0].size();
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (height[i][j] == 0) {
-                    height[i][j] = 1;
-                } else {
-                    height[i][j] = 0;
+                if (height[i][j] == 1) {
+                    waters.push_back({i, j});
                 }
             }
         }
-        bool changed = true;
-        int max = 0;
-        while (changed) {
-            changed = false;
-            for (int i = 0; i < m; i++) {
-                for (int j = 0; j < n; j++) {
-                    int temp = height[i][j];
-                    if ((temp != 0 || temp > max) && (i == 0 || height[i - 1][j] >= temp) && (i + 1 == m || height[i+1][j] >= temp) && (j == 0 || height[i][j - 1] >= temp) && (j + 1 == n || height[i][j + 1] >= temp)) {
-                        height[i][j]++;
-                        changed = true;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int min = INT_MAX;
+                for (auto k : waters) {
+                    int dist = abs(k[0] - i) + abs(k[1] - j);
+                    if (dist < min) {
+                        min = dist;
                     }
                 }
+                height[i][j] = min;
             }
-            max++;
         }
-
         return height;
     }
 };
