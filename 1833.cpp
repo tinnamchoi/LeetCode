9792bfe618fb4c55ac5d9422b02822732bcd7b25
tinnamchoi <tinnam.choi@gmail.com>
2023-01-06 17:37:00 +0800

    1833-A

--- 1833.cpp
+++ 1833.cpp
@@ -11,13 +11,13 @@ public:
     int maxIceCream(vector<int>& costs, int coins) {
         sort(costs.begin(), costs.end());
         for (int i = 0; i < costs.size(); i++) {
+            coins -= costs[i];
             if (coins < 0) {
-                return i - 1;
-            }
-            if (coins == 0) {
                 return i;
             }
-            coins -= costs[i];
+            if (coins == 0) {
+                return i + 1;
+            }
         }
         return costs.size();
     }
