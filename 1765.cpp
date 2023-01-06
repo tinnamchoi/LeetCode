75cb4694aead296d0e4697175922abbfbacab20d
tinnamchoi <tinnam.choi@gmail.com>
2023-01-06 19:19:00 +0800

    1765-RE

--- 1765.cpp
+++ 1765.cpp
@@ -23,14 +23,16 @@ public:
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 int min = INT_MAX;
-                for (auto k : waters) {
-                    if (min == 0 || height[i][j] == 1) {
-                        min = 0;
-                        break;
-                    }
-                    int dist = abs(k[0] - i) + abs(k[1] - j);
-                    if (dist < min) {
-                        min = dist;
+                if (isWater[i][j] == 1) {
+                    min = 0;
+                } else if ((i != 0 && isWater[i - 1][j]) || (i + 1 != m && isWater[i + 1][j]) || (j != 0 && isWater[i][j - 1]) || (j + 1 != m && isWater[i][j + 1])) {
+                    min = 1;
+                } else {
+                    for (auto k : waters) {
+                        int dist = abs(k[0] - i) + abs(k[1] - j);
+                        if (dist < min) {
+                            min = dist;
+                        }
                     }
                 }
                 height[i][j] = min;
