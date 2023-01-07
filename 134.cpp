4a51299e30be8cde13a7aac4ac56569787b12d70
tinnamchoi <tinnam.choi@gmail.com>
2023-01-07 15:43:00 +0800

    134-A

--- 134.cpp
+++ 134.cpp
@@ -12,19 +12,20 @@ public:
         int n = gas.size();
         int diff = 0;
         vector<int> v(n);
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) { // O(n)
             int temp = gas[i] - cost[i];
             diff += temp;
             v[i] = temp;
         }
-        if (diff < 0) {
+        if (diff < 0) { // O(1)
             return -1;
         }
-        for (int i = 0; i < n; i++) {
-            if (v[i] < 0) {
+        for (int i = 0; i < n; i++) { // O(n)
+            if (v[i] < 0 || gas[i] == 0) {
                 continue;
             }
             int tank = 0;
+            // O(n) 
             for (int j = i; j < n; j++) {
                 tank += v[j];
                 if (tank < 0) {
