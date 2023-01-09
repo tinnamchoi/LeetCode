0b08ed59786b48ca2417c1e1a4bbd6b125b222fa
tinnamchoi <tinnam.choi@gmail.com>
2023-01-10 02:38:00 +0800

    144-WA

---
+++ 144.cpp
@@ -0,0 +1,51 @@
+/* Binary Tree Preorder Traversal */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+/* Submission */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        if (root == nullptr) {
+            return {};
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+        vector<int> out;
+        while (!q.empty()) {
+            TreeNode* current = q.front();
+            q.pop();
+            out.push_back(current->val);
+            if (current->left != nullptr) {
+                q.push(current->left);
+            }
+            if (current->right != nullptr) {
+                q.push(current->right);
+            }
+        }
+        return out;
+    }
+};
