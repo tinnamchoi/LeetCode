b8b741500b366d654ea58825ccbdbcac3a5c8a4d
tinnamchoi <tinnam.choi@gmail.com>
2023-01-12 03:56:00 +0800

    77-A

--- 77.cpp
+++ 77.cpp
@@ -11,14 +11,14 @@ public:
     vector<vector<int>> combine(int n, int k) {
         vector<vector<int>> out;
         if (k == 1) {
-            for (int i = 1; i <= n; i++) {
+            for (int i = 1; i <= n; i++) { // O(N)
                 out.push_back({i});
             }
             return out;
         }
         vector<vector<int>> in = combine(n, k - 1);
-        for (auto i : in) {
-            for (int j = i.back() + 1; j <= n; j++) {
+        for (auto i : in) { //O(N)
+            for (int j = i.back() + 1; j <= n; j++) { // O(N)
                 vector<int> temp = i;
                 temp.push_back(j);
                 out.push_back(temp);
