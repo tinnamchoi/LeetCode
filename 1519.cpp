61a515fff44dfe92596f584122709b7b34a6dbdb
tinnamchoi <tinnam.choi@gmail.com>
2023-01-13 00:44:00 +0800

    1519-TLE

--- 1519.cpp
+++ 1519.cpp
@@ -10,7 +10,7 @@ class Solution {
 public:
     vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
         vector<int> parent(n, -1);
-        for (auto i : edges) {
+        for (auto i : edges) { // O(E)
             if (i[0] == 0) {
                 parent[i[1]] = 0;
                 continue;
@@ -25,13 +25,10 @@ public:
             }
             parent[i[1]] = i[0];
         }
-        for (auto i : parent) {
-            cout << i << endl;
-        }
         vector<int> ans(n, 1);
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < n; i++) { // O(V)
             int temp = i;
-            while (parent[temp] != -1) {
+            while (parent[temp] != -1) { // O(V)
                 temp = parent[temp];
                 if (labels[temp] == labels[i]) {
                     ans[temp]++;
