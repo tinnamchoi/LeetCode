03472c4b35f3afc3fd6e4dd0f10a3ce5774cce78
tinnamchoi <tinnam.choi@gmail.com>
2023-01-14 01:41:00 +0800

    2246-WA

---
+++ 2246.cpp
@@ -0,0 +1,55 @@
+/* Longest Path With Different Adjacent Characters */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Submission */
+
+class Solution {
+public:
+    int ans = 0;
+    vector<int> parent;
+    vector<vector<int>> child;
+    string s;
+
+    int dfs(int i, int count) {
+        int max = 0;
+        int max2 = 0;
+        if (i == 0 || s[parent[i]] == s[i]) {
+            count = 1;
+        } else {
+            count++;
+            if (count > ans) {
+                ans = count;
+            }
+        }
+        for (int j : child[i]) {
+            int temp = dfs(j, count);
+            if (s[j] != s[i]) {
+                if (temp >= max) {
+                    max2 = max;
+                    max = temp;
+                } else if (temp > max2) {
+                    max2 = temp;
+                }
+                temp = max + max2 + 1;
+                if (temp > ans) {
+                    ans = temp;
+                }
+            }
+        }
+        return max + 1;
+    }
+    int longestPath(vector<int>& parent, string s) {
+        this->parent = parent;
+        int n = parent.size();
+        this->s = s;
+        child.resize(n);
+        for (int i = 1; i < n; i++) {
+            child[parent[i]].push_back(i);
+        }
+        dfs(0, 0);
+        return ans;
+    }
+};
