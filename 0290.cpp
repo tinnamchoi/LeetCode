23a9a478727f8889e25359b6da1ae839c4c0be9f
tinnamchoi <tinnam.choi@gmail.com>
2023-01-14 18:57:00 +0800

    290-A

--- 0290.cpp
+++ 0290.cpp
@@ -9,6 +9,7 @@ using namespace std;
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
+        // unordered_map is more efficient than just map
         unordered_map<char, int> cmap;
         unordered_map<string, int> smap;
         
@@ -16,13 +17,13 @@ public:
         int n = pattern.size();
         int i = 0;
 
-        for (string word; iss >> word; i++) {
-            if (cmap[pattern[i]] != smap[word]) {
+        for (string word; iss >> word; i++) { // O(m): Loop while iss is not empty, m is number of words in s
+            if (i == n || cmap[pattern[i]] != smap[word]) { // If there are more words than the length of pattern OR the letter and word aren't mapped to the same integer
                 return false;
             }
-            cmap[pattern[i]] = smap[word] = i + 1;
+            cmap[pattern[i]] = smap[word] = i + 1; // Map to i + 1 to prevent mapping different words to the same integer
         }
 
-        return true;
+        return i == n; // If the lengths are incremented the same, then i should've been incremented n times
     }
 };
