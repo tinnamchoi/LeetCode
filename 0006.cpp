4fea56f16d5dd59334e091fbb62ce88dc68a96c0
tinnamchoi <tinnam.choi@gmail.com>
2023-02-04 05:00:00 +0800

    0006: A

--- 0006.cpp
+++ 0006.cpp
@@ -30,7 +30,7 @@ public:
                 // Using a temp variable to check whether the index is out of range
                 int temp = j * loop + i;
                 if (temp < n) {
-                    ans.append(1, s[temp]);
+                    ans.push_back((char)s[temp]);
                 }
                 // No slanted bit to add if we're on either the top or bottom row
                 if (i == 0 || i == numRows - 1) {
@@ -39,7 +39,7 @@ public:
                 // Adding the slanted bit
                 temp = (j + 1) * loop - i;
                 if (temp < n) {
-                    ans.append(1, s[temp]);
+                    ans.push_back((char)s[temp]);
                 } 
             }
         }
