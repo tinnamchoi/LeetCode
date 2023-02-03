3ebab4f5501b54a694910d82fa12ef9f729c5731
tinnamchoi <tinnam.choi@gmail.com>
2023-02-04 04:48:00 +0800

    0006: A

--- 0006.cpp
+++ 0006.cpp
@@ -16,19 +16,26 @@ public:
         int loop = numRows * 2 - 2;
 
         // Store the number of times the pattern repeats
+        // Adding (n % loop != 0) gives the ceiling of (n / loop)
         int iter = n / loop + (n % loop != 0);
+
         string ans;
 
+        // Since we are reading this row-by-row
+        // We'll iterate through the rows in the outer loop
         for (int i = 0; i < numRows; i++) {
+            // We'll then iterate through each repeat of the pattern
             for (int j = 0; j < iter; j++) {
-                if (j * loop + i < n) {
-                    ans.append(1, s[j * loop + i]);
+                int temp = j * loop + i;
+                if (temp < n) {
+                    ans.append(1, s[temp]);
                 } 
                 if (i == 0 || i == numRows - 1) {
                     continue;
                 }
-                if ((j + 1) * loop - i < n) {
-                    ans.append(1, s[(j + 1) * loop - i]);
+                temp = (j + 1) * loop - i;
+                if (temp < n) {
+                    ans.append(1, s[temp]);
                 } 
             }
         }
