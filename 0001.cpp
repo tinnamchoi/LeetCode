5ef8c9bc31e821d58053b4544adb82953295a196
tinnamchoi <tinnam.choi@gmail.com>
2023-02-09 20:23:00 +0800

    1-A
    Some optimizations and adding comments

--- 0001.cpp
+++ 0001.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        // Store the indexes of the numbers that we've encountered thus far
         unordered_map<int, int> umap;
+        
         int n = nums.size();
+
+        // Iterate through the array
         for (int i = 0; i < n; i++) {
-            if (umap[target - nums[i]] != 0) {
-                return {umap[target - nums[i]] - 1, i};
+            // If the complement (target - current number) had already been encountered
+            if (umap.find(target - nums[i]) != umap.end()) {
+                // Return the index of the complement and the current index
+                return {umap[target - nums[i]], i};
             }
-            umap[nums[i]] = i + 1;
+            // Else store the index of the current number
+            umap[nums[i]] = i;
         }
+
+        // Although the test cases are generated to always have a solution, 
+        // we still need this just so that the code compiles
         return {};
     }
 };
