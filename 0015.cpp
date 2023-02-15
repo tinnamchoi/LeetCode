8018a506578e799cfeed5d0bcdbc32162129fcbe
tinnamchoi <tinnam.choi@gmail.com>
2023-02-16 00:59:00 +0800

    0015: TLE

--- 0015.cpp
+++ 0015.cpp
@@ -4,19 +4,20 @@ public:
         unordered_set<int> sums;
         int n = nums.size();
         set<vector<int>> s;
+        vector<vector<int>> ans = {};
         for (int i = 0; i < n - 2; i++) {
             sums.insert(nums[i]);
             for (int j = i + 2; j < n; j++) {
                 if (sums.find(0 - nums[i + 1] - nums[j]) != sums.end()) {
                     vector<int> temp = {nums[i + 1], nums[j], 0 - nums[i + 1] - nums[j]};
                     sort(temp.begin(), temp.end());
+                    if (s.find(temp) != s.end()) {
+                        continue;
+                    }
+                    ans.push_back(temp);
                     s.insert(temp);
                 }
             }
-        }
-        vector<vector<int>> ans = {};
-        for (auto & i : s) {
-            ans.push_back(i);
         }
         return ans;
     }
