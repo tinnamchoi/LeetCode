625d64bf84ee42d6a1b7049d45ec55ae12049d5c
tinnamchoi <tinnam.choi@gmail.com>
2023-02-15 23:43:00 +1030

    0015: TLE

--- 0015.cpp
+++ 0015.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        set<int> sums;
         int n = nums.size();
         set<vector<int>> s;
         for (int i = 0; i < n - 2; i++) {
-            for (int j = i + 1; j < n - 1; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if (nums[i] + nums[j] + nums[k] == 0) {
-                        vector<int> temp = {nums[i], nums[j], nums[k]};
-                        sort(temp.begin(), temp.end());
-                        s.insert(temp);
-                    }
+            sums.insert(nums[i]);
+            for (int j = i + 2; j < n; j++) {
+                if (sums.find(0 - nums[i + 1] - nums[j]) != sums.end()) {
+                    vector<int> temp = {nums[i + 1], nums[j], 0 - nums[i + 1] - nums[j]};
+                    sort(temp.begin(), temp.end());
+                    s.insert(temp);
                 }
             }
         }
