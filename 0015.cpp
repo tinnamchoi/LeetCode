8e35502dd56e08e27f296a716659a40de074ae08
tinnamchoi <tinnam.choi@gmail.com>
2023-02-16 19:16:00 +0800

    0015: TLE

--- 0015.cpp
+++ 0015.cpp
@@ -1,35 +1,22 @@
-/* duplicate*/
-
 class Solution {
 public:
-    string vint2string(vector<int> nums) {
-        string ans = "";
-        ans += to_string(nums[0]);
-        ans += " ";
-        ans += to_string(nums[1]);
-        ans += " ";
-        ans += to_string(nums[2]);
-        return ans;
-    }
-
     vector<vector<int>> threeSum(vector<int>& nums) {
         unordered_set<int> sums;
         int n = nums.size();
-        unordered_set<string> s;
-        vector<vector<int>> ans = {};
+        set<vector<int>> s;
         for (int i = 0; i < n - 2; i++) {
             sums.insert(nums[i]);
             for (int j = i + 2; j < n; j++) {
                 if (sums.find(0 - nums[i + 1] - nums[j]) != sums.end()) {
                     vector<int> temp = {nums[i + 1], nums[j], 0 - nums[i + 1] - nums[j]};
                     sort(temp.begin(), temp.end());
-                    if (s.find(vint2string(temp)) != s.end()) {
-                        continue;
-                    }
-                    ans.push_back(temp);
-                    s.insert(vint2string(temp));
+                    s.insert(temp);
                 }
             }
+        }
+        vector<vector<int>> ans = {};
+        for (auto & i : s) {
+            ans.push_back(i);
         }
         return ans;
     }
