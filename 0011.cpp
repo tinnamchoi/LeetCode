608265ca41fdae337843869483e2a775a293115d
tinnamchoi <tinnam.choi@gmail.com>
2023-03-11 22:37:00 +1030

    0011: TLE

--- 0011.cpp
+++ 0011.cpp
@@ -3,28 +3,12 @@ public:
     int maxArea(vector<int>& height) {
         int n = height.size();
 
-        int l = 0;
-        int r = n - 1;
-
         int ans = INT_MIN;
 
-        while (l < r) {
-            ans = max(ans, min(height[l], height[r]) * (r - l));
-
-            int l_diff = height[l] - height[l + 1];
-            int r_diff = height[r] - height[r - 1];
-
-            if (l_diff > r_diff) {
-                r--;
-                continue;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                ans = max(ans, (j - i) * min(height[i], height[j]));
             }
-            if (r_diff > l_diff) {
-                l++;
-                continue;
-            }
-
-            l++;
-            r--;
         }
 
         return ans;
