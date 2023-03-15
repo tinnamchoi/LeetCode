1ee1474e19507a346b485ad0068f09f884f18292
tinnamchoi <tinnam.choi@gmail.com>
2023-03-15 14:40:00 +1030

    3-WA
    Failed to consider edge case with string size of 0

---
+++ 0003.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+
+        unordered_set<char> exists;
+        int ans = INT_MIN;
+
+        int l = 0;
+
+        for (int i = 0; i < n; i++) {
+            ans = max(ans, i - l);
+            if (exists.find(s[i]) != exists.end()) {
+                while (s[l] != s[i]) {
+                    exists.erase(s[l]);
+                    l++;
+                }
+                exists.erase(s[l]);
+                l++;
+            }
+            exists.insert(s[i]);
+        }
+
+        return ans;
+    }
+};
