62fdf55dbcb175d0ea6fe91b6d00996603b6458e
tinnamchoi <tinnam.choi@gmail.com>
2023-04-25 02:12:00 +0930

    5-WA
    Messed up trying to fix the edge case

--- 0005.cpp
+++ 0005.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
+        cout << s << endl;
         int n = s.size();
         int ans_index = 0;
         int ans_size = 1;
         for (int i = 1; i < n; i++) {
-            int size = -1;
-            for (int j = 0; i - j >= 0 && i + j < n; j++) {
-                if (s[i - j] == s[i + j]) {
-                    size = j;
-                    continue;
+            {
+                int size = -1;
+                for (int j = 0; i - j >= 0 && i + j < n; j++) {
+                    if (s[i - j] == s[i + j]) {
+                        size = j;
+                        continue;
+                    }
+                    if (ans_size < j * 2 - 1) {
+                        ans_index = i - j;
+                        ans_size = j * 2 - 1;
+                    }
+                    size = -1;
+                    break;
                 }
-                if (ans_size < j * 2 - 1) {
-                    ans_index = i - j;
-                    ans_size = j * 2 - 1;
+                size++;
+                if (size && ans_size < size * 2 - 1) {
+                    ans_index = i - size + 1;
+                    ans_size = size * 2 - 1;
                 }
-                size = -1;
-                break;
-            }
-            if (size && ans_size < size * 2 - 1) {
-                ans_index = i - size;
-                ans_size = size * 2 - 1;
             }
             if (s[i - 1] == s[i]) {
                 int size = -1;
