43c3cc2db3193d0c1a77d0e20c6ec0cdfc4835a1
tinnamchoi <tinnam.choi@gmail.com>
2023-04-25 00:03:00 +0930

    4-A
    O(lg(m + n)) Optimal binary-search submission after reading solutions, ends up having longer run time

--- 0004.cpp
+++ 0004.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size();
-        int n = nums2.size();
-        int num1;
-        int num2;
-        int j1 = 0;
-        int j2 = 0;
-        for (int i = 0; i <= (m + n) / 2; i++) {
-            if (j1 < m && (j2 >= n || nums1[j1] <= nums2[j2])) {
-                num1 = num2;
-                num2 = nums1[j1];
-                j1++;
-            } else {
-                num1 = num2;
-                num2 = nums2[j2];
-                j2++;
-            }
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        
+        if (n2 > n1) return findMedianSortedArrays(nums2, nums1);
+
+        int l = 0;
+        int r = n2 * 2;
+
+        while (l <= r) {
+            int m2 = l + (r - l) / 2;
+            int m1 = n1 + n2 - m2;
+
+            int l1 = m1 ? nums1[(m1 - 1) / 2] : INT_MIN;
+            int l2 = m2 ? nums2[(m2 - 1) / 2] : INT_MIN;
+            int r1 = m1 != n1 * 2 ? nums1[m1 / 2] : INT_MAX;
+            int r2 = m2 != n2 * 2 ? nums2[m2 / 2] : INT_MAX;
+
+            if (r2 < l1) l = m2 + 1;
+            else if (l2 > r1) r = m2 - 1;
+            else return (double)(max(l1, l2) + min(r1, r2)) / 2;
         }
-        return (m + n) % 2 ? num2 : (double)(num1 + num2) / 2;
+
+        return -1;
     }
 };
