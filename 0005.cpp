0802620ff67f675d89359d7d40a3929148c7204b
tinnamchoi <tinnam.choi@gmail.com>
2023-04-25 02:00:00 +0930

    5-WA
    Failed attempt at fixing edge cases

--- 0005.cpp
+++ 0005.cpp
@@ -5,22 +5,41 @@ public:
         int ans_index = 0;
         int ans_size = 1;
         for (int i = 1; i < n; i++) {
+            int size = -1;
             for (int j = 0; i - j >= 0 && i + j < n; j++) {
-                if (s[i - j] == s[i + j]) continue;
+                if (s[i - j] == s[i + j]) {
+                    size = j;
+                    continue;
+                }
                 if (ans_size < j * 2 - 1) {
                     ans_index = i - j;
                     ans_size = j * 2 - 1;
                 }
+                size = -1;
                 break;
             }
+            if (size && ans_size < size * 2 - 1) {
+                ans_index = i - size;
+                ans_size = size * 2 - 1;
+            }
             if (s[i - 1] == s[i]) {
-                for (int j = 0; i - j >= 0 && i + j < n; j++) {
-                    if (s[i - 1 - j] == s[i + j]) continue;
+                int size = -1;
+                for (int j = 0; i - 1 - j >= 0 && i + j < n; j++) {
+                    if (s[i - 1 - j] == s[i + j]) {
+                        size = j;
+                        continue;
+                    }
                     if (ans_size < j * 2) {
                         ans_index = i - j;
                         ans_size = j * 2;
                     }
+                    size = -1;
                     break;
+                }
+                size++;
+                if (size && ans_size < size * 2) {
+                    ans_index = i - size;
+                    ans_size = size * 2;
                 }
             }
         }
