bccf85d4c12d9926bb08d6c8fca4ad5af8520890
tinnamchoi <tinnam.choi@gmail.com>
2023-04-25 17:37:00 +0930

    0016: TLE

--- 0016.cpp
+++ 0016.cpp
@@ -2,13 +2,24 @@ class Solution {
 public:
     int threeSumClosest(vector<int>& nums, long target) {
         int n = nums.size();
-        long ans = target > 0 ? INT_MIN : INT_MAX;
+        long ans = target < 0 ? INT_MAX : INT_MIN;
+        sort(nums.begin(), nums.end());
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    long sum = nums[i] + nums[j] + nums[k];
-                    ans = max(sum, target) - min (sum, target) < max(ans, target) - min(ans, target) ? sum : ans;
+            int l = i + 1;
+            int r = n - 1;
+            while (l < r) {
+                long sum = nums[i] + nums[l] + nums[r];
+                cout << "ans: " << ans << ", sum: " << sum << endl;
+                ans = max(ans, target) - min(ans, target) > max(sum, target) - min(sum, target) ? sum : ans;
+                if (sum > target) {
+                    r--;
+                    continue;
                 }
+                if (sum < target) {
+                    l++;
+                    continue;
+                }
+                return target;
             }
         }
         return ans;
