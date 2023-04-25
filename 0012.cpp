440c01c5653af0fc3ef4184385f14693a02e882a
tinnamchoi <tinnam.choi@gmail.com>
2023-04-25 14:05:00 +0930

    0012: A

--- 0012.cpp
+++ 0012.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        vector<string> di = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-        vector<string> te = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-        vector<string> hu = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-        vector<string> th = {"", "M", "MM", "MMM"};
+        string di[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        string te[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        string hu[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        string th[] = {"", "M", "MM", "MMM"};
 
         return th[num % 10000 / 1000] + hu[num % 1000 / 100] + te[num % 100 / 10] + di[num % 10];        
     }
