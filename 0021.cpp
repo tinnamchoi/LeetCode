3bc8fc5b24b73245d92482df8fb5f93e093a045e
tinnamchoi <tinnam.choi@gmail.com>
2023-04-26 20:18:00 +0930

    0021: A
    This was in a mock btw

--- 0021.cpp
+++ 0021.cpp
@@ -11,6 +11,12 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (!list1) {
+            return list2;
+        }
+        if (!list2) {
+            return list1;
+        }
         ListNode* head;
         if (list1->val < list2->val) {
             head = list1;
