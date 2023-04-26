bb8508868a27f2d95661701910bed51f4f6b82ae
tinnamchoi <tinnam.choi@gmail.com>
2023-04-26 12:07:00 +0930

    258-A
    Very easy daily, 00:01:07 PB

---
+++ 0258.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum >= 10 ? addDigits(sum) : sum;
+    }
+};
