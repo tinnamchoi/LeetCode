02a4f55596b6f49cf54fbddde8318db686e4f021
tinnamchoi <tinnam.choi@gmail.com>
2023-04-26 19:05:00 +0930

    0018: RE
    Integer overflow

---
+++ 0018.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        set<vector<int>> ans;
+        unordered_map<int, set<vector<int>>> umap;
+        for (int b = 1; b < n - 2; b++) {
+            for (int a = 0; a < b; a++) {
+                vector<int> temp;
+                temp.push_back(nums[a]);
+                temp.push_back(nums[b]);
+                sort(temp.begin(), temp.end());
+                int lhs = nums[a] + nums[b];
+                umap[lhs].insert(temp);
+            }
+            int c = b + 1;
+            for (int d = b + 2; d < n; d++) {
+                int rhs = target - nums[c] - nums[d];
+                for (auto v : umap[rhs]) {
+                    v.push_back(nums[c]);
+                    v.push_back(nums[d]);
+                    sort(v.begin(), v.end());
+                    ans.insert(v);
+                }
+            }
+        }
+        vector<vector<int>> ret;
+        for (auto quad : ans) {
+            ret.push_back(quad);
+        }
+        return ret;
+    }
+};
