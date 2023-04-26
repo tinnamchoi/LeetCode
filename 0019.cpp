a5f7cf074be6c3dc7f58221861749df2a439d03d
tinnamchoi <tinnam.choi@gmail.com>
2023-04-26 20:03:00 +0930

    0019: A
    Tried to improve it by removing some unnecessary logic, got worse

--- 0019.cpp
+++ 0019.cpp
@@ -12,18 +12,18 @@ class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         ListNode* fast = head;
-        ListNode* slow = head;
         for (int i = 0; i < n; i++) {
             fast = fast->next;
         }
-        if (fast == nullptr) {
+        if (!fast) {
             return head->next;
         }
+        ListNode* slow = head;
         while (fast->next) {
             fast = fast->next;
             slow = slow->next;
         }
-        slow->next = slow->next == nullptr ? nullptr : slow->next->next;
+        slow->next = slow->next ? slow->next->next : nullptr;
         return head;
     }
 };
