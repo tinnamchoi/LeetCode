c038012b0496c874116dcc0368227a58dfbc1455
tinnamchoi <tinnam.choi@gmail.com>
2023-04-26 21:27:00 +0930

    0024: WA
    I'm so bad at edge cases

---
+++ 0024.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (!head || !head->next) {
+            return head;
+        }
+        ListNode* ret = head->next;
+        
+        ListNode* curr = head;
+        ListNode* then = head->next;
+
+        while (then) {
+            ListNode* new_curr = then->next;
+            ListNode* new_then = then->next ? then->next->next : nullptr;
+            curr->next = new_then;
+            then->next = curr;
+            curr = new_curr;
+            then = new_then;
+        }
+
+        return ret;
+    }
+};
