a74cd0f2a02166ef1514c19b53ab56c1f21e869a
tinnamchoi <tinnam.choi@gmail.com>
2023-04-26 12:32:00 +0930

    0258-A
    I realized I didn't even need a helper function

--- 0258.cpp
+++ 0258.cpp
@@ -1,17 +1,13 @@
 class Solution {
-private:
-    int digit_sum(int num) {
-        int sum = 0;
-        while (num > 0) {
-            sum += num % 10;
-            num /= 10;
-        }
-        return sum;
-    }
 public:
     int addDigits(int num) {
         while (num >= 10) {
-            num = digit_sum(num);
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
         }
         return num;
     }
