bf04cc5ba206ddb76f78d096b286cf311c4bcc9f
tinnamchoi <tinnam.choi@gmail.com>
2023-04-26 12:14:00 +0930

    0258-A
    Iterative solution with helper function and comparison against 9

--- 0258.cpp
+++ 0258.cpp
@@ -1,11 +1,18 @@
 class Solution {
-public:
-    int addDigits(int num) {
+private:
+    int digit_sum(int num) {
         int sum = 0;
         while (num > 0) {
             sum += num % 10;
             num /= 10;
         }
-        return sum >= 10 ? addDigits(sum) : sum;
+        return sum;
+    }
+public:
+    int addDigits(int num) {
+        while (num > 9) {
+            num = digit_sum(num);
+        }
+        return num;
     }
 };
